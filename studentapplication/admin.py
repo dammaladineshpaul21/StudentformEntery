993f81d57fa4092aad4588e96bcc9031373173d1
dammaladineshpaul21 <dammaladineshpaul21@gmail.com>
2023-02-05 04:10:49 +0530

    Add Table CreateUser and studentform in models.py

---
+++ studentapplication/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from studentapplication.models import *
+
+# Register your models here.
+admin.site.register(StudentApplication)
+admin.site.register(Department)
+admin.site.register(CreateUserProfile)
